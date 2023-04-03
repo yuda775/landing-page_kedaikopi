cef814adc762056b2e8cc2d83c5f6a271576e034
yuda775 <saputra.yuda775@gmail.com>
2023-04-03 09:45:47 +0700

    fresh finish

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementsByTagName('nav')[0];
+window.addEventListener('scroll', function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 30) {
+        navbar.classList.add('nav-color');
+    } else {
+        navbar.classList.remove('nav-color');
+    }
+})
